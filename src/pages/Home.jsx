e9206b1841f35c3c931149cdc1d20b96475e17db
Rasul <jolyskytner@gmail.com>
2022-11-19 17:34:58 +0500

    added pagination function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Pizza } from "../components/Pizza";
+import ReactPaginate from 'react-paginate';
 import { Sort } from "../components/Sort";
 import { Categories } from "../components/Categories";
 import Skeleton from "../components/Pizza/Skeleton";
+import { Pagination } from "../components/Pagination.jsx";
 export const Home = ({ searchValue }) => {
   const [pizzaItems, setPizzaItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
@@ -12,7 +14,7 @@ export const Home = ({ searchValue }) => {
     sortProperty: "rating",
   });
   const [categoryId, setCategoryId] = React.useState(0);
-
+  const [currentPage, setCurrentPage] = useState(1);
   useEffect(() => {
     setIsLoading(true);
     const order = sort.sortProperty.includes("-") ? "desc" : "asc";
@@ -22,14 +24,14 @@ export const Home = ({ searchValue }) => {
 
     async function fetchdata() {
       const PizzasData = await axios.get(
-        `https://6375ff1eb5f0e1eb85ff4f3c.mockapi.io/Pizzas?${search}&${category}&sortBy=${replacing}&order=${order}`
+        `https://6375ff1eb5f0e1eb85ff4f3c.mockapi.io/Pizzas?page=${currentPage}&limit=4&${search}&${category}&sortBy=${replacing}&order=${order}`
       );
       setPizzaItems(PizzasData.data);
       setIsLoading(false);
     }
     fetchdata();
     window.scrollTo(0, 0);
-  }, [categoryId, sort, searchValue]);
+  }, [categoryId, sort, searchValue, currentPage]);
 
   const pizzas = pizzaItems.map((obj) => <Pizza key={obj.id} {...obj} />);
   const skeleton = [...new Array(6)].map((_, index) => (
@@ -46,6 +48,7 @@ export const Home = ({ searchValue }) => {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">{isLoading ? skeleton : pizzas}</div>
+      <Pagination onChangePage={(number => setCurrentPage(number))}/>
     </div>
   );
 };
