ec1defaf07be30ab65927821818918265e58c98d
Rasul <jolyskytner@gmail.com>
2022-11-19 16:52:37 +0500

    search function added

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Cart } from "./pages/Cart";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
+import { useContext } from "react";
 
 function App() {
+  const [searchValue, setSearchValue] = useState("");
   return (
     <div className="App">
       <div className="wrapper">
-        <Header />
+        <Header searchValue={searchValue} setSearchValue={setSearchValue} />
 
         <div className="content">
           <div className="container">
-      
             <Routes>
-                <Route path="/" element={<Home />}/>
-                <Route path="/cart" element={<Cart/>}/>
-                <Route path="*" element={<NotFound/>} />
-              </Routes>
+              <Route path="/" element={<Home searchValue={searchValue} />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
           </div>
         </div>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Pizza } from "../components/Pizza";
 import { Sort } from "../components/Sort";
 import { Categories } from "../components/Categories";
 import Skeleton from "../components/Pizza/Skeleton";
-export const Home = () => {
+export const Home = ({ searchValue }) => {
   const [pizzaItems, setPizzaItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [sort, setSort] = useState({
@@ -14,22 +14,27 @@ export const Home = () => {
   const [categoryId, setCategoryId] = React.useState(0);
 
   useEffect(() => {
-    const order = sort.sortProperty.includes('-') ? 'desc' : 'asc'
-    const replacing = sort.sortProperty.replace('-','')
-    const category = categoryId > 0 ? `category=${categoryId}` : " "
     setIsLoading(true);
+    const order = sort.sortProperty.includes("-") ? "desc" : "asc";
+    const replacing = sort.sortProperty.replace("-", "");
+    const category = categoryId > 0 ? `category=${categoryId}` : " ";
+    const search = searchValue ? `title=${searchValue}` : " ";
+
     async function fetchdata() {
       const PizzasData = await axios.get(
-        `https://6375ff1eb5f0e1eb85ff4f3c.mockapi.io/Pizzas?${
-          category
-        }&sortBy=${replacing}&order=${order}`
+        `https://6375ff1eb5f0e1eb85ff4f3c.mockapi.io/Pizzas?${search}&${category}&sortBy=${replacing}&order=${order}`
       );
       setPizzaItems(PizzasData.data);
       setIsLoading(false);
     }
     fetchdata();
     window.scrollTo(0, 0);
-  }, [categoryId, sort]);
+  }, [categoryId, sort, searchValue]);
+
+  const pizzas = pizzaItems.map((obj) => <Pizza key={obj.id} {...obj} />);
+  const skeleton = [...new Array(6)].map((_, index) => (
+    <Skeleton key={index} />
+  ));
   return (
     <div className="container">
       <div className="content__top">
@@ -40,11 +45,7 @@ export const Home = () => {
         <Sort value={sort} onClickSort={(index) => setSort(index)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {isLoading
-          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-          : pizzaItems.map((obj) => <Pizza key={obj.id} {...obj} />)}
-      </div>
+      <div className="content__items">{isLoading ? skeleton : pizzas}</div>
     </div>
   );
 };
